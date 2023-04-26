6d2d2d9c16190ad898181ddcde5e4f902ffa8225
kalen510 <kalen.wiley@gmail.com>
2023-04-26 15:18:58 -0400

    display planner elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // get current date (formatted)
 var today = moment().format("dddd, MMMM Do");
-console.log(today);
 // display current date to header
 $("#currentDay").text(today);
 
@@ -15,8 +14,27 @@ $("#currentDay").text(today);
     // assign id attribute to time
     // 3 columns per row (time, textarea, save button)
 var displayPlanner = function() {
+    for (i = 6; i < 23; i++) {
+        var hourRow =  $("<div>").addClass("row");
 
-}
+        var hourTitle = $("<div>")
+            .addClass("col-2")
+            .text(moment().hour(i).format("h A"));
+
+        var hourTasks = $("<textarea>")
+            .addClass("col-8")
+            .attr("id", i);
+
+        var saveBtn =  $("<button>").addClass("col-2");
+
+        // append columns to row
+        hourRow.append(hourTitle, hourTasks, saveBtn);
+        // append row to container
+        $(".container").append(hourRow);
+
+    }
+
+};
 
 
 // load tasks from localStorage on load
@@ -24,5 +42,5 @@ var loadTasks = function() {
 
 };
 
-// displayPlanner();
+displayPlanner();
 // loadTasks();
