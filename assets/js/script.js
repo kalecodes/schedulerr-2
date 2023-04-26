946168999ca48892593187d2ba70c4680a45dd9f
kalen510 <kalen.wiley@gmail.com>
2023-04-25 21:31:11 -0400

    js comments

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// use setInterval to periodically update style of textareas
+
+// create row for each hour block
+    // assign id attribute to time
+    // 3 columns per row (time, textarea, save button)
+
+// save tasks to localStorage
+
+
+// load tasks from localStorage on load
+
+
