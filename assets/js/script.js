4520a9dfbcd06a15785f5312511390be076c9453
kalen510 <kalen.wiley@gmail.com>
2023-04-26 15:01:19 -0400

    display current date to header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
+// get current date (formatted)
+var today = moment().format("dddd, MMMM Do");
+console.log(today);
+// display current date to header
+$("#currentDay").text(today);
+
 // use setInterval to periodically update style of textareas
 
-// create row for each hour block
-    // assign id attribute to time
-    // 3 columns per row (time, textarea, save button)
+
 
 // save tasks to localStorage
 
 
+// create row for each hour block
+    // assign id attribute to time
+    // 3 columns per row (time, textarea, save button)
+var displayPlanner = function() {
+
+}
+
+
 // load tasks from localStorage on load
+var loadTasks = function() {
 
+};
 
+// displayPlanner();
+// loadTasks();
